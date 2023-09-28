2394cec4383dd8d9751c2892d24969ce0fc2b6b7
Ankit Thakur <ankitthakur@192.168.1.14>
2023-09-28 18:30:12 +0530

    fixing ui

--- src/libs/ui/TravelOS/Components/pages/section/home/filterComponent/filterItem/filterItemone/FilterItemOne.tsx
+++ src/libs/ui/TravelOS/Components/pages/section/home/filterComponent/filterItem/filterItemone/FilterItemOne.tsx
@@ -37,73 +37,68 @@ const FilterItemOne = () => {
           />
         </Box>
       </Box>
-      <Card sx={{ borderRadius: '19px', border: '2px solid #0057B7',maxWidth:'589px' }}>
-        <Grid container spacing={3} alignItems="center">
-          <Grid item xs={12} sm={4}>
-            <CardMedia
-              component="img"
-              sx={{
-                width: '100%',
-                borderRadius: '16px 0 0 16px',
-                height: '100%',
-                objectFit: 'fill',
-              }}
-              image="https://images.pexels.com/photos/13610654/pexels-photo-13610654.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-              alt="Image Alt Text"
-            />
+      <Card
+      sx={{
+        borderRadius: '19px',
+        border: '2px solid #0057B7',
+        maxWidth: '589px',
+        width: '100%', // To make it responsive
+      }}
+    >
+      <Grid container spacing={3}>
+        {/* Image Section */}
+        <Grid item xs={12} sm={4}>
+          <CardMedia
+            component="img"
+            sx={{
+              width: '100%',
+              borderRadius: '16px 0 0 16px',
+              height: '100%',
+              objectFit: 'cover', // Updated for better responsiveness
+            }}
+            image="https://images.pexels.com/photos/13610654/pexels-photo-13610654.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt="Image Alt Text"
+          />
+        </Grid>
+        {/* Content Section */}
+        <Grid item xs={12} sm={8} container>
+          <Grid item xs={12} sm={6}>
+            <CardContent>
+              <Typography variant='subtitle2' gutterBottom color="primary">
+                ATLANTIS, THE PALM
+              </Typography>
+              <Box sx={{ mt: 0, lineHeight: 0.66 }}>
+                <Typography color="text.secondary" variant='caption'>
+                  A daring destination resort where you’ll be left with a feeling of wonderment.
+                </Typography>
+              </Box>
+              <Rating
+                name="simple-controlled"
+                value={3} // Set your initial rating value here
+                readOnly // Disable rating interactivity for mobile
+                sx={{ color: 'primary.main' }} // Change the star color
+              />
+            </CardContent>
           </Grid>
-          <Grid item xs={12} sm={8} container>
-            <Grid item xs={12} sm={6}>
-              <CardContent
-                className='card-content-paragraph'
-              >
-                <Typography
-                  mt={2}
-                  variant='subtitle2'
-                  // component="div"
-                  gutterBottom
-                  color="primary"
-                >
-                  ATLANTIS, THE PALM
-                </Typography>
-                <Box sx={{ mt: 0, lineHeight: 0.66, }}>
-                  <Typography
-                    color="text.secondary"
-                    variant='caption' >
-                    A daring destination resort
-                    where you’ll be left with a
-                    feeling of wonderment.                  </Typography>
-                </Box>
-                <Rating
-        name="simple-controlled"
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      />              </CardContent>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <Paper
-                elevation={0}
-                sx={{ display: 'flex', flexDirection: 'column', p: 2 }}
-              >
-                <Typography variant="body2" color="text.secondary">
-                  Daily Rates From
-                </Typography>
-                <Typography variant="h5" color="primary">
-                  <b>AED 2,125</b>
-                </Typography>
-                <Typography variant="caption" color="text.secondary">
-                  exc taxes and fees
-                </Typography>
-                <Button variant="outlined" color="primary" sx={{ mt: 1, width: '113px', height: '30px', fontSize: '11px' }}>
-                  View Rooms
-                </Button>
-              </Paper>
-            </Grid>
+          <Grid item xs={12} sm={6}>
+            <Paper elevation={0} sx={{ display: 'flex', flexDirection: 'column', p: 2 }}>
+              <Typography variant="body2" color="text.secondary">
+                Daily Rates From
+              </Typography>
+              <Typography variant="h5" color="primary">
+                <b>AED 2,125</b>
+              </Typography>
+              <Typography variant="caption" color="text.secondary">
+                exc taxes and fees
+              </Typography>
+              <Button variant="outlined" color="primary" sx={{ mt: 1, fontSize: '11px' }}>
+                View Rooms
+              </Button>
+            </Paper>
           </Grid>
         </Grid>
-      </Card>
+      </Grid>
+    </Card>
     </Box>
   );
 };
